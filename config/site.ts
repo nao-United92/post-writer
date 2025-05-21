a5077a93156d70367ed7c4688a5d45a758870e7b
nao-United92 <naoyauver@icloud.com>
2025-05-21 23:27:46 +0900

    add siteConfig

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from '@/types';
+
+export const siteConfig: SiteConfig = {
+  name: 'Post Writer',
+  description: '',
+  url: 'http://localhost:3000',
+  ogImage: '',
+  links: {
+    x: 'https://x.com/s_uvernao',
+    github: 'https://github.com/nao-United92',
+  },
+};
