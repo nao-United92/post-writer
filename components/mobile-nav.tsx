7110a1e50fcf6f13f69faaa757c49db15bfa8ad5
nao-United92 <naoyauver@icloud.com>
2025-05-28 00:27:47 +0900

    call useLockBodyScroll()

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -1,5 +1,6 @@
 import { siteConfig } from '@/config/site';
 import { NavItem } from '@/types';
+import { useLockBodyScroll } from '@uidotdev/usehooks';
 import Link from 'next/link';
 
 interface MobileNavProps {
@@ -7,15 +8,19 @@ interface MobileNavProps {
 }
 
 export default function MobileNav({ items }: MobileNavProps) {
+  useLockBodyScroll();
+
   return (
-    <div className='fixed top-16 inset-0 z:50 p-6 shadow-md md:hidden animate-in slide-in-from-bottom-80'>
-      <div className='grid gap-6  bg-popover p-4 text-popover-foreground shadow-md'>
+    <div className="fixed top-16 inset-0 z:50 p-6 shadow-md md:hidden animate-in slide-in-from-bottom-80">
+      <div className="grid gap-6  bg-popover p-4 text-popover-foreground shadow-md">
         <Link href={'/'} className="font-bold">
           {siteConfig.name}
         </Link>
-        <nav className='text-sm flex gap-4 '>
+        <nav className="text-sm flex gap-4 ">
           {items.map((item, index) => (
-            <Link key={index} href={item.href}>{item.title}</Link>
+            <Link key={index} href={item.href}>
+              {item.title}
+            </Link>
           ))}
         </nav>
       </div>
