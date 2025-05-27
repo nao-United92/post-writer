dcdd020cfb3a564fec6cc082e7c50604c9a717e2
nao-United92 <naoyauver@icloud.com>
2025-05-28 00:23:00 +0900

    add contact section

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -174,6 +174,25 @@ export default function IndexPage() {
           </p>
         </div>
       </section>
+
+      <section id="contact" className="container py-8 md:py-12 lg:py-24">
+        <div className="max-w-[58rem] mx-auto text-center flex flex-col gap-4">
+          <h2 className="font-extrabold text-3xl md:text-6xl">Contact Me</h2>
+          <p className="text-muted-foreground sm:text-lg sm:leading-7">
+            もしもWebサービスが気に入った場合は下記XからDMでご連絡ください。
+            <br />
+            お仕事のご連絡をお待ちしております。
+          </p>
+          <Link
+            href={siteConfig.links.x}
+            className="underline underline-offset-4"
+            target="_blank"
+            rel="noreferrer"
+          >
+            お仕事はXまで
+          </Link>
+        </div>
+      </section>
     </>
   );
 }
