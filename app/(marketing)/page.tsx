7c629c3820bb87da396cbbab505e5457fdd1ab25
nao-United92 <naoyauver@icloud.com>
2025-05-27 23:52:28 +0900

    add technical stack tab section

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -53,7 +53,7 @@ export default function IndexPage() {
           </p>
         </div>
         <div>
-          <div className="bg-background">
+          <div className="bg-background border p-2 rounded-lg">
             <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -63,6 +63,10 @@ export default function IndexPage() {
               >
                 <path d="M64 0C28.7 0 0 28.7 0 64s28.7 64 64 64c11.2 0 21.7-2.9 30.8-7.9L48.4 55.3v36.6h-6.8V41.8h6.8l50.5 75.8C116.4 106.2 128 86.5 128 64c0-35.3-28.7-64-64-64zm22.1 84.6l-7.5-11.3V41.8h7.5v42.8z" />
               </svg>
+              <div>
+                <h3>Next.js</h3>
+                <p>AppRouter/Layouts/APIRoutesの技術を使用。</p>
+              </div>
             </div>
           </div>
         </div>
