ebcb6cfabac72490f5e6b64e12e1b33009bef9dd
nao-United92 <naoyauver@icloud.com>
2025-06-05 00:26:15 +0900

    add Image&Callout to mdx-component.tsx

--- components/mdx-component.tsx
+++ components/mdx-component.tsx
@@ -1,11 +1,18 @@
 import { useMDXComponent } from 'next-contentlayer/hooks';
+import Image from 'next/image';
+import Callout from './callout';
+
+const components = {
+  Image,
+  Callout,
+};
 
 export default function Mdx({ code }: { code: string }) {
   const Component = useMDXComponent(code);
 
   return (
     <div>
-      <Component />
+      <Component components={components} />
     </div>
   );
 }
