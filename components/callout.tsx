3f2ce6abdc800c7bb3d6afa0a3122433203665ac
nao-United92 <naoyauver@icloud.com>
2025-06-05 00:29:40 +0900

    add callout.tsx

--- components/callout.tsx
+++ components/callout.tsx
@@ -1,3 +1,7 @@
-export default function Callout() {
-  return <div>Hello</div>;
+export default function Callout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="my-6 flex items-center rounded-md border border-l-4 p-4">
+      <div>{children}</div>
+    </div>
+  );
 }
