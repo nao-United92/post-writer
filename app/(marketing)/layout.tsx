2739405846fb4d7cd120d402d85b103696061c82
nao-United92 <naoyauver@icloud.com>
2025-06-07 00:50:21 +0900

    change login button size to lg

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -19,7 +19,7 @@ export default function MarketingLayout({
             <Link
               href={'/login'}
               className={cn(
-                buttonVariants({ variant: 'secondary', size: 'sm' }),
+                buttonVariants({ variant: 'secondary', size: 'lg' }),
                 'px-4'
               )}
             >
