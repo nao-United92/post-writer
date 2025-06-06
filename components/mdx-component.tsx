46d61c1a899d319d73d0b6b455e28fdd293b80a3
nao-United92 <naoyauver@icloud.com>
2025-06-07 00:21:19 +0900

    implement tailwindcss/typography

--- components/mdx-component.tsx
+++ components/mdx-component.tsx
@@ -11,7 +11,7 @@ export default function Mdx({ code }: { code: string }) {
   const Component = useMDXComponent(code);
 
   return (
-    <div>
+    <div className="prose lg:prose-xl max-w-full">
       <Component components={components} />
     </div>
   );
