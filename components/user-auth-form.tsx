7402e07acf2c642aeef731e74bd00592e478212a
nao-United92 <naoyauver@icloud.com>
2025-06-08 00:41:11 +0900

    add page-tsx for login and register

---
+++ components/user-auth-form.tsx
@@ -0,0 +1,13 @@
+export default function UserAuthForm() {
+  return (
+    <div>
+      <form>
+        <div>
+          <div>
+            <Label></Label>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
