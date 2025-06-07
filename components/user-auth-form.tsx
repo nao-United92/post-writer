325f02ed7a5bd0671e337967bb0f32372a8d8110
nao-United92 <naoyauver@icloud.com>
2025-06-08 00:48:21 +0900

    add user-auth-form

--- components/user-auth-form.tsx
+++ components/user-auth-form.tsx
@@ -1,11 +1,20 @@
+import { cn } from '@/lib/utils';
+import { buttonVariants } from './ui/button';
+import { Input } from './ui/input';
+import { Label } from './ui/label';
+
 export default function UserAuthForm() {
   return (
     <div>
       <form>
         <div>
           <div>
-            <Label></Label>
+            <Label htmlFor="email">メールアドレス</Label>
+            <Input id="email" placeholder="name@example.com" type="email" />
           </div>
+          <button className={cn(buttonVariants())}>
+            メールアドレスでログイン
+          </button>
         </div>
       </form>
     </div>
