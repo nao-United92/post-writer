702880ae48144d6d6b4b8506e7a23633de451722
nao-United92 <naoyauver@icloud.com>
2025-06-08 01:02:14 +0900

    style to github login

--- components/user-auth-form.tsx
+++ components/user-auth-form.tsx
@@ -5,10 +5,10 @@ import { Label } from './ui/label';
 
 export default function UserAuthForm() {
   return (
-    <div>
+    <div className="grid gap-6">
       <form>
-        <div>
-          <div>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
             <Label htmlFor="email">メールアドレス</Label>
             <Input id="email" placeholder="name@example.com" type="email" />
           </div>
@@ -17,6 +17,21 @@ export default function UserAuthForm() {
           </button>
         </div>
       </form>
+
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs">
+          <span className="text-muted-foreground px-2 bg-background">
+            または
+          </span>
+        </div>
+      </div>
+
+      <button className={cn(buttonVariants({ variant: 'outline' }))}>
+        Github
+      </button>
     </div>
   );
 }
