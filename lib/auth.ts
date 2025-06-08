a5d6ec57e4d3cd10f71aa791326273267e373f81
nao-United92 <naoyauver@icloud.com>
2025-06-09 00:22:31 +0900

    add authOption for Github

---
+++ lib/auth.ts
@@ -0,0 +1,11 @@
+import { NextAuthOptions } from 'next-auth';
+import Github from 'next-auth/providers/github';
+
+export const authOption: NextAuthOptions = {
+  providers: [
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+};
