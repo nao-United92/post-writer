19e52afc7395df9e6b41b5698d71625a74e0362c
nao-United92 <naoyauver@icloud.com>
2025-06-18 23:15:20 +0900

    create POST API

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -2,6 +2,12 @@ import { authOption } from '@/lib/auth';
 import { db } from '@/lib/db';
 import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
+import { z } from 'zod';
+
+const postCreateSchema = z.object({
+  title: z.string(),
+  content: z.string().optional(),
+});
 
 export async function POST(req: NextRequest) {
   try {
@@ -14,14 +20,26 @@ export async function POST(req: NextRequest) {
     const { user } = session;
 
     const json = await req.json();
+    const body = postCreateSchema.parse(json);
+    const { title, content } = body;
 
     const post = await db.post.create({
       data: {
-        title: 'Untitled',
-        content: '',
-        published: false,
+        title,
+        content,
         authorId: user.id,
       },
+      select: {
+        id: true,
+      },
     });
-  } catch (err) {}
+
+    return NextResponse.json(post);
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      return NextResponse.json(err.issues, { status: 422 });
+    }
+
+    return NextResponse.json(null, { status: 500 });
+  }
 }
