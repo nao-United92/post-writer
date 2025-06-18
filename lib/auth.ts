ad4a77895930683b5cf3a76413ef35b66f3dbc80
nao-United92 <naoyauver@icloud.com>
2025-06-18 23:29:40 +0900

    add async jwt to lib/auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -21,6 +21,14 @@ export const authOption: NextAuthOptions = {
     signIn: '/login',
   },
   callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        return { ...token, id: user.id };
+      }
+
+      return token;
+    },
+
     async session({ token, session }) {
       if (token) {
         session.user.id = token.id;
