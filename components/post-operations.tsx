12d49051623932aa47f18eeec05cf4c8be0e3b88
nao-United92 <naoyauver@icloud.com>
2025-06-21 00:37:33 +0900

    add postDelete function

--- components/post-operations.tsx
+++ components/post-operations.tsx
@@ -20,12 +20,35 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from './ui/alert-dialog';
+import { toast } from 'sonner';
+import { useRouter } from 'next/navigation';
+
+async function deletePost(postId: string) {
+  try {
+    const response = await fetch(`/api/posts/${postId}`, {
+      method: 'DELETE',
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed');
+    }
+
+    return true;
+  } catch (err) {
+    toast({
+      title: '問題が発生しました。',
+      description: '記事の削除ができませんでした。もう一度お試しください。',
+      variant: 'destructive',
+    });
+  }
+}
 
 interface PostOperationsProps {
   post: Pick<Post, 'id' | 'title'>;
 }
 
 export default function PostOperations({ post }: PostOperationsProps) {
+  const router = useRouter();
   const [showDeleteAlert, setShowDeleteAlert] = useState<boolean>(false);
 
   return (
@@ -59,7 +82,19 @@ export default function PostOperations({ post }: PostOperationsProps) {
           </AlertDialogHeader>
           <AlertDialogFooter>
             <AlertDialogCancel>キャンセル</AlertDialogCancel>
-            <AlertDialogAction>削除</AlertDialogAction>
+            <AlertDialogAction
+              onClick={async (e) => {
+                e.preventDefault();
+                const deleted = await deletePost(post.id);
+
+                if (deleted) {
+                  setShowDeleteAlert(false);
+                  router.refresh();
+                }
+              }}
+            >
+              削除
+            </AlertDialogAction>
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
