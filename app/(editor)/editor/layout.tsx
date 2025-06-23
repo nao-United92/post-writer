9075d0b0f7308b646aaaf80d210fd5e4fae911d9
nao-United92 <naoyauver@icloud.com>
2025-06-23 23:50:24 +0900

    delete unnecessary's import

--- app/(editor)/editor/layout.tsx
+++ app/(editor)/editor/layout.tsx
@@ -1,11 +1,8 @@
 import DashboardNav from '@/components/dashboard-nav';
 import MainNav from '@/components/main-nav';
 import SiteFooter from '@/components/site-footer';
-import { buttonVariants } from '@/components/ui/button';
 import { dashboardConfig } from '@/config/dashboard';
 import { marketingConfig } from '@/config/marketing';
-import { cn } from '@/lib/utils';
-import Link from 'next/link';
 
 export default function EditorLayout({
   children,
